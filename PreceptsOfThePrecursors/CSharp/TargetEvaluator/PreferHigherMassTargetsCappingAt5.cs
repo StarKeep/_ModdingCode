e53123e444cae4856199ef7a13a316f7ae0503c0
StarKelp <fergusonstf@yahoo.com>
2020-08-23 21:25:30 -0500

    Regnat Update Starts with the ability to pull in small groups of strikecraft and immediately metabolize them. Gains more shots per mark. At mark 3, gains the ability to also pull in and rapidly consume units of mass 5 or less. At mark 5, upgrades both weapons to Greater Metabolization. Added in a new targeting optimizer that will make it prefer higher mass targets for its secondary weapon.

---
+++ PreceptsOfThePrecursors/CSharp/TargetEvaluator/PreferHigherMassTargetsCappingAt5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arcen.AIW2.Core;
+using Arcen.AIW2.External;
+using Arcen.Universal;
+
+namespace PreceptsOfThePrecursors.TargetEvaluator
+{
+    public class PreferHigherMassTargetsCappingAt5 : ITargetEvaluatorImplementation
+    {
+        public float CalculateNormalizedAdditionToCoreImportance( GameEntity_Squad AttackerEntity, EntitySystem AttackerSystem, GameEntity_Squad DefenderEntity, bool priorityAIBased )
+        {
+            if ( DefenderEntity.TypeData.Mass_tX > 5 )
+                return 0;
+
+            if ( AttackerSystem.TypeData.BaseKnockbackPerShot != 0 && AttackerSystem.GetKnockbackPowerAgainst( DefenderEntity ) == 0 )
+                return 0; // If we're supposed to pull/push something, and we can't, we don't otherwise care.
+
+            return (TargetListPlanning.FACTOR_IMPORTANCE_FOR_ADDED_MINOR_EFFECT / 5) * DefenderEntity.TypeData.Mass_tX.GetNearestIntPreferringHigher();
+        }
+    }
+}
